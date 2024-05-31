6ed37e725fc3034df1b99b9e8de3ab37f7b04afe
shfpflyawei <34828547+shfpflyawei@users.noreply.github.com>
2024-05-31 14:20:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
-s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd  && tail -f /dev/null
+s3fs "$S3_BUCKET" "$MNT_POINT" url="$S3_URL"-o passwd_file=passwd  && tail -f /dev/null
